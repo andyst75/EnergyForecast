3faaef32782b8ae3bb246dda55c84c7c442c6e05
nickdn <nickdndev@gmail.com>
2020-12-22 22:19:45 +0300

    add plot for energy consumption

--- energy.py
+++ energy.py
@@ -2,14 +2,15 @@
 Работа с моделью и данными по электроэнергетике ОЭС Средняя Волга
 """
 import pickle
-import pandas as pd
+
 import numpy as np
+
 
 class Energy:
 
     def __init__(self, filename='model_data.pkl'):
         """Загружаем заранее предобработанные данные и предобученную модель"""
-        
+
         with open(filename, 'rb') as handle:
             self.data = pickle.load(handle)
             self.df = self.data['data']
@@ -19,15 +20,14 @@ class Energy:
         """
         Датафрейм, начиная с указанной даты
         """
-        
+
         return np.expm1(self.df[self.df.DATE >= date][["USE_FACT"]])
 
-    def get_predict(self, date):
+    def get_data_with_consumption(self, date):
         """
         Датафрейм, содержащий прогнозы, начиная с указанной даты, на последующие 5 дней
         """
-        
-        dataframe = data['data']
-        X_data = self.df[self.df.DATE >= date] \
-            .drop(columns=["DATE", "USE_PRED1", "USE_PRED2", "USE_PRED3", "USE_PRED4", "USE_PRED5"])
-        return np.expm1(data['model'].predict(X_data))
+        drop_columns = ["DATE", "USE_PRED1", "USE_PRED2", "USE_PRED3", "USE_PRED4", "USE_PRED5"]
+        filtered_data = self.df[self.df.DATE >= date].drop(columns=drop_columns)
+        filtered_data['consumption'] = np.expm1(self.data['model'].predict(filtered_data)[:, 2])
+        return filtered_data
--- energy_app.py
+++ energy_app.py
@@ -1,18 +1,11 @@
-import sys
-import numpy as np
-import pandas as pd
-
-import streamlit as st
-import matplotlib.pyplot as plt
-from pandas.plotting import register_matplotlib_converters
-import seaborn as sns
 import datetime
-import base64
-import time
-import os
-import pickle
 
-from Energy import Energy
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+from pandas.plotting import register_matplotlib_converters
+
+from energy import Energy
 
 register_matplotlib_converters()
 plt.style.use('default')
@@ -21,10 +14,57 @@ st.set_page_config(layout='centered')
 
 st.title('Energy forecast app')
 
-energy = Energy()
+st.markdown('''
+This app retrieves cryptocurrency prices for the top 100 cryptocurrency from the **CoinMarketCap**!
+''')
 
-df = energy.get_data()
+expander_bar = st.beta_expander("About")
+expander_bar.markdown("""
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn, BeautifulSoup, requests, json, time
+* **Data source:** [Minenergo](http://https://minenergo.gov.ru/).
+""")
+DAYS_BACK = 200
+PRED_HORIZON = 7
+WIN_LEN = 30
+TODAY = datetime.datetime.now().date()
+MIN_DATE = TODAY - datetime.timedelta(days=DAYS_BACK) + datetime.timedelta(days=WIN_LEN)
+MAX_DATE = datetime.datetime(2020, 10, 10).date()
 
-df['USE_FACT'].plot(figsize=(10, 5), label='past')
+st.sidebar.header('User Input Features')
 
+col1 = st.sidebar
+col1.header('Input Options')
+predict_period = col1.slider('Predict period', value=7, min_value=1, max_value=7)
+
+random_date = col1.checkbox('Random date', value=False)
+
+if random_date:
+    delta = int((MAX_DATE - MIN_DATE).days * np.random.random())
+    predict_from = MIN_DATE + datetime.timedelta(days=delta)
+else:
+    predict_from = MAX_DATE
+predict_from = col1.date_input('Predict from', value=predict_from, min_value=MIN_DATE, max_value=MAX_DATE)
+
+col1.button('Update')
+
+
+@st.cache
+def load_data():
+    return Energy()
+
+data_df = load_data()
+
+df_with_consumption = data_df.get_data_with_consumption(str(predict_from))
+
+ax = df_with_consumption['consumption'].plot(figsize=(10, 5), label='predict')
+ax.set(ylabel=f'consumption', xlabel='', title=f'Prediction of the consumption')
+plt.legend()
 st.pyplot(plt)
+
+with st.beta_expander("See explanation"):
+    st.write("""
+           The chart above shows some numbers I picked for you.
+            I rolled actual dice for these, so they're *guaranteed* to
+            be random.
+         """)
+    st.image("https://static.streamlit.io/examples/dice.jpg")
